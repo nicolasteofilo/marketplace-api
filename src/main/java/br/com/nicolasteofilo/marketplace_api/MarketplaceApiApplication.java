1b44e1e61fb21271991048cbf5f81e60286ab3f8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2026-02-03 19:07:51 -0300

    feat: add GET / hello world

--- src/main/java/br/com/nicolasteofilo/marketplace_api/MarketplaceApiApplication.java
+++ src/main/java/br/com/nicolasteofilo/marketplace_api/MarketplaceApiApplication.java
@@ -2,9 +2,17 @@ package br.com.nicolasteofilo.marketplace_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MarketplaceApiApplication {
+	@GetMapping("/")
+	public String helloWord() {
+		return "Welcome to Marketplace ó";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceApiApplication.class, args);
 	}
